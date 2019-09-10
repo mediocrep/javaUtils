e8268333d98bf8f26f140f8aa72e77086314716b
Witt Zhang <zhangwei3583@126.com>
2019-09-11 00:20:09 +0800

    commit 2: random util

--- src/main/java/com/utils/StringUtils.java
+++ src/main/java/com/utils/StringUtils.java
@@ -1,17 +1,19 @@
 package com.utils;
 
 import com.utils.constants.Constants;
+import org.testng.annotations.Test;
 
 import java.util.Random;
+import java.util.UUID;
 
 /**
  * String工具类
  */
 public class StringUtils {
     /**
-     * 根据传递过来的参数capacity，获取capacity长度的随机字符串
+     * 根据传递过来的参数capacity，获取指定长度的随机字符串（方法1）
      * @param capacity
-     * @return
+     * @return String
      */
     public static String getRandomStr(int capacity){
         Random random = new Random();
@@ -23,6 +25,45 @@ public class StringUtils {
         }
         return sb.toString();
     }
+
+    /**
+     * 根据传递过来的参数length，获取指定长度的随机字符串（方法2）
+     * @param length
+     * @return String
+     */
+    public static String getRandomStr2(int length){
+        //1.  定义一个字符串（A-Z，a-z，0-9）即62个数字字母；
+        String str="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789*!@#$%^&*()_+<>?";
+        //2.  由Random生成随机数
+        Random random=new Random();
+        StringBuffer sb=new StringBuffer();
+        //3.  长度为几就循环几次
+        for(int i=0; i<length; ++i){
+            //从62个的数字或字母中选择
+            int number=random.nextInt(62);
+            //将产生的数字通过length次承载到sb中
+            sb.append(str.charAt(number));
+        }
+        //将承载的字符转换成字符串
+        return sb.toString();
+    }
+
+    /**
+     * 生成32位随机字符串，小写字母a - f，和数字组成（方法3）
+     * @return String
+     */
+    public static String getUUID() {
+        return UUID.randomUUID().toString().trim().replaceAll("-", "");
+    }
+
+    /**
+     * 获取指定长度的随机字符串（方法4）：
+     * commons-lang.jar包下有一个RandomStringUtils类，
+     *  (1)其中有一个randomAlphanumeric(int length)函数，可以随机生成一个长度为length的字符串
+     *      RandomStringUtils.randomAlphanumeric(32)
+     *  (2)RandomStringUtils.random(32,true,false) 获取包含字母、不含数字的32位随机字符串
+     */
+
 
     /**
      * 判断字符串是否为空：null或""
--- src/main/java/com/utils/test/Test.java
+++ src/main/java/com/utils/test/Test.java
@@ -1,6 +1,7 @@
 package com.utils.test;
 
 import com.utils.StringUtils;
+import org.apache.commons.lang.RandomStringUtils;
 
 public class Test {
 
@@ -13,7 +14,11 @@ public class Test {
         System.out.println(StringUtils.isEmpty(" ad ")); //false
         System.out.println(StringUtils.isEmpty("  ")); //true
 
+        System.out.println(StringUtils.getUUID());
 
+        System.out.println(RandomStringUtils.randomAlphanumeric(10));
+        System.out.println(RandomStringUtils.random(10,true,false));
+        System.out.println(RandomStringUtils.random(10,false,true));
 
     }
 }
