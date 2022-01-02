40eb886b4442676580eb67dfb7cf9e11c509a52b
witt <zhangwei3583@126.com>
2022-01-02 18:13:07 +0800

    add m3u8 function

---
+++ src/main/java/com/util/m3u8/listener/DownloadListener.java
@@ -0,0 +1,13 @@
+package com.util.m3u8.listener;
+
+public interface DownloadListener {
+
+    void start();
+
+    void process(String downloadUrl, int finished, int sum, float percent);
+
+    void speed(String speedPerSecond);
+
+    void end();
+
+}
